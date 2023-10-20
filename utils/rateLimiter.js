41b26ec6c4471101c5022a1d5b1e30f839c38c1d
Max Logvinov <maxlogvinov93@gmail.com>
2023-10-20 04:24:09 +0300

    hotfix

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
   windowMs: 2 * 1000,
-  max: 4,
+  max: 2,
   standardHeaders: true,
   legacyHeaders: false,
 });
